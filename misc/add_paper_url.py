ec37716cdc4e8ed47849b18fa06db7c919868f51
Arin Tsikretsis <adt608@hotmail.com>
2014-06-08 01:22:31 -0500

    requirements

--- misc/add_paper_url.py
+++ misc/add_paper_url.py
@@ -1,1 +1,22 @@
 __author__ = 'arin'
+import json
+import os
+from django.db import connection
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'DAC_network_analysis.local_settings')
+def add_urls_to_papers(filename):
+
+    papers = json.load(open(filename))
+    cursor = connection.cursor()
+    cursor.execute('ALTER TABLE Papers ADD url VARCHAR(300)')
+    cursor.close()
+    for paper in papers:
+        cursor = connection.cursor()
+        cursor.execute('UPDATE Papers SET url = %s WHERE DOI = %s',[paper['url'].encode('UTF-8'),paper['doi'].encode('UTF-8')])
+        cursor.close()
+def remove_url():
+    cursor = connection.cursor()
+    cursor.execute('ALTER TABLE Papers DROP url')
+    cursor.close()
+
+# add_urls_to_papers("papers.json")
+remove_url()
--- topic_modelling/insert_into_top5.py
+++ topic_modelling/insert_into_top5.py
@@ -1,1 +1,28 @@
 __author__ = 'arin'
+import csv
+import os
+from django.db import connection
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'DAC_network_analysis.local_settings')
+def insert_csv(filename):
+        reader = csv.reader(open(filename))
+        cursor = connection.cursor()
+        cursor.execute(
+            'DROP TABLE IF EXISTS TopFives;'
+            'CREATE TABLE TopFives(parentId INT, childId INT, rank INT);'
+        )
+        cursor.close()
+        i = 0
+
+        for line in reader:
+            print i
+            i+=1
+            cursor = connection.cursor()
+            cursor.execute(
+                'INSERT INTO TopFives (parentId, childId, rank) VALUES '
+                '('
+                '(SELECT PaperId FROM Papers WHERE DOI = %s),'
+                '(SELECT PaperId FROM Papers WHERE DOI = %s),'
+                '%s );',[line[0].encode('UTF-8'),line[1].encode('UTF-8'),line[2].encode('UTF-8')])
+            cursor.close()
+
+insert_csv('top5.csv')
