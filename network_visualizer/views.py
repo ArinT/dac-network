d0428321021df4c433270459e66ee12231aac967
Arin Tsikretsis <adt608@hotmail.com>
2014-06-08 22:18:53 -0500

    Added query for author info and paper info

--- network_visualizer/views.py
+++ network_visualizer/views.py
@@ -4,13 +4,11 @@ from django.template import RequestContext, loader
 from django import forms
 from django.shortcuts import render_to_response, redirect
 from network_visualizer.query_database import *
-query_list = [get_author_info,get_paper_info()]
+query_list = [get_author_info,get_paper_info]
 def index(request):
     template = loader.get_template('index.html')
     context = RequestContext(request)
     return HttpResponse(template.render(context))
-
-
 def proto1(request):
     template = loader.get_template('proto1.html')
     context = RequestContext(request)
@@ -32,13 +30,14 @@ def query_author(request):
     if request.method == 'POST':
         query = QueryForm(request.POST)
         if query.is_valid():
-            context = get_results_context(query, request, 0)
+            context = query_list[0](query['query'].value())
+            return context
 def query_paper(request):
-    pass
-def get_results_context(query, request, type):
-    query_results = query_list[type](query['query'].value())
-    objects = []
-    return 0
+    if request.method == 'POST':
+        query = QueryForm(request.POST)
+        if query.is_valid():
+            context = query_list[1](query['query'].value())
+            return context
 # Create your views here.
 class QueryForm(forms.Form):
     query = forms.IntegerField()
