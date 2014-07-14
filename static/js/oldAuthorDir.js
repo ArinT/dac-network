85579d060b60de1b7a7283409abcab68bfa7f9b4
nknordeen <nknordeen@gmail.com>
2014-07-14 16:21:44 -0500

    added arrows to the graphs, and changed the charge to a much lower value

--- static/js/oldAuthorDir.js
+++ static/js/oldAuthorDir.js
@@ -1,16 +1,23 @@
 app.controller("authorGraphCtrl", function($scope){
 	$scope.typeGraph = "degreeCentrality";
-	$scope.chosenScore = 0.2;
-	$scope.popoverBody = "<a href='#'>hello</a>"
+	$scope.chosenScore = 0;
+	$scope.loaded = false;
 	$scope.$watch("chosenScore", function(val, oldVal){
 		if(val !== oldVal){
+			$scope.loaded = false;
 			$scope.$broadcast("NewGraph");
 		}
 	});
 	$scope.$watch("typeGraph", function(newVal, oldVal){
 		if(newVal !== oldVal){
+			$scope.loaded = false;
 			$scope.$broadcast("NewGraph");
 		}
+	});
+	$scope.$on("GraphLoaded", function(){
+		$scope.$apply(function(){
+			$scope.loaded = true;
+		});
 	});
 });
 
@@ -19,9 +26,10 @@ app.directive("authorGraph", function(){
 		restrict:"A",
 		controller:"authorGraphCtrl",
 		link:function(scope, elem, attrs){
-			var fileName = "../../static/json/authors_centrality.json";
+			var fileName = "../../static/json/authors.json";
 			var dom = "#author-graph";
-			drawGraph(scope, scope.chosenScore,"degreeCentrality",fileName, dom, -100, "AuthorNodeClicked");
+			drawGraph(scope, scope.chosenScore,scope.typeGraph,fileName, dom, -100, "AuthorNodeClicked");
+			
 			scope.$on("NewGraph",function(){
 				$("svg").remove();
 	  			drawGraph(scope, scope.chosenScore, scope.typeGraph,fileName, dom, -100, "AuthorNodeClicked");
@@ -29,15 +37,3 @@ app.directive("authorGraph", function(){
 		}//end link
 	}//end return
 });
-function chooseCentrality(typeCent){
-	if(typeCent === "degreeCentrality"){
-		return "degree";
-	}
-	if(typeCent === "closenessCentrality"){
-		return "closeness";
-	}
-	if(typeCent === "betweennessCentrality"){
-		return "betweenness";
-	}
-}//end chooseCentrality()
-
--- static/js/oldGraph.js
+++ static/js/oldGraph.js
@@ -1,5 +1,5 @@
 
-function drawGraph(scope, isCitationNetwork, score, centrality, jsonFile, domId, charge, nodeClicked){
+function drawGraph(scope, score, centrality, jsonFile, domId, charge, nodeClicked){
 	var width = $(domId).width();
 	var height = $(window).height();
 	if( $("#menu") !== null ){
@@ -9,8 +9,8 @@ function drawGraph(scope, isCitationNetwork, score, centrality, jsonFile, domId,
 	var color = d3.scale.category20b();
 
 	var force = d3.layout.force()
-	    .charge(-500)
-	    .linkDistance(70)
+	    .charge(-400)
+	    .linkDistance(25)
 	    .size([width, height]);
 	    force.gravity(0.6);
 	    
@@ -21,38 +21,12 @@ function drawGraph(scope, isCitationNetwork, score, centrality, jsonFile, domId,
 		})
 		.attr("viewBox", "0 0 " + width + " " + height )
 		.attr("preserveAspectRatio", "xMidYMid meet")
-		.append("g")
-	    .call(d3.behavior.zoom().scaleExtent([-3, 8]).on("zoom", zoom))
+	    .append("g")
+	    .call(d3.behavior.zoom().scaleExtent([-4, 8]).on("zoom", zoom))
 	    .append("g");
-	    
-	if (isCitationNetwork){
-		svg.append('svg:defs').append('svg:marker')
-		    .attr('id', 'end-arrow')
-		    .attr('viewBox', '0 -5 10 10')
-		    .attr('refX', 6)
-		    .attr('markerWidth', 5)
-		    .attr('markerHeight', 5)
-		    .attr('orient', 'auto')
-		  .append('svg:path')
-		    .attr('d', 'M0,-5L10,0L0,5')
-		    .attr('fill', '#000');
-
-		svg.append('svg:defs').append('svg:marker')
-		    .attr('id', 'start-arrow')
-		    .attr('viewBox', '0 -5 10 10')
-		    .attr('refX', 4)
-		    .attr('markerWidth', 5)
-		    .attr('markerHeight', 5)
-		    .attr('orient', 'auto')
-		  .append('svg:path')
-		    .attr('d', 'M10,-5L0,0L10,5')
-		    .attr('fill', '#000');
-	}
-
 	function zoom(){
 		svg.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
 	}//end zoom()
-
 	setTimeout(function(){
 	d3.json(jsonFile, function(error, graph) {
 		var edgeArr = [];
@@ -62,24 +36,9 @@ function drawGraph(scope, isCitationNetwork, score, centrality, jsonFile, domId,
 		if(score !== null){
 			//removing the edges that are between a node with centrality lower than the one specified.
 			for(var i = 0; i<graph.links.length; i++){
-				var edge = graph.links[i];			
-				var src = graph.nodes[edge.source];
-				var tgt = graph.nodes[edge.target];
-				
-				if(src[centrality] >= score && tgt[centrality]>= score){
-					if (edge.year == undefined)
-					{
-						edgeArr.push(graph.links[i]);
-					}
-					else
-					{
-						START = "2002";
-						END = "2012";
-						if (edge.year >= START && edge.year <= END)
-						{
-							edgeArr.push(graph.links[i]);
-						}					
-					}
+				if(graph.nodes[graph.links[i].source][centrality] >= score 
+					&& graph.nodes[graph.links[i].target][centrality]>= score){
+					edgeArr.push(graph.links[i]);
 				}
 			}
 			for(var i = 0; i<edgeArr.length; i++){
@@ -99,35 +58,31 @@ function drawGraph(scope, isCitationNetwork, score, centrality, jsonFile, domId,
 			.nodes(graph.nodes)
 			.links(graph.links)
 			.start();
-	    
+	    	console.log(1);
 	    for(var i = 160; i>0; --i){
 	    	force.tick();
 	    }
 	    force.stop();
 	    scope.$broadcast("GraphLoaded");
-		var link = svg.append('svg:g').selectAll("path")
+		var link = svg.selectAll(".link")
 			.data(graph.links)
-			.enter().append("path")
+			.enter().append("line")
 			.attr("class", "link")
-			.attr("d",function(d){
-				var deltaX = d.target.x - d.source.x,
-				deltaY = d.target.y - d.source.y,
-				dist = Math.sqrt(deltaX * deltaX + deltaY * deltaY),
-				normX = deltaX / dist,
-				normY = deltaY / dist,
-				sourcePadding = 0; //d.left ? 17 : 12,
-				targetPadding =  12;// : 12,
-				sourceX = d.source.x + (sourcePadding * normX),
-				sourceY = d.source.y + (sourcePadding * normY),
-				targetX = d.target.x - (targetPadding * normX),
-				targetY = d.target.y - (targetPadding * normY);
-				return 'M' + sourceX + ',' + sourceY + 'L' + targetX + ',' + targetY;
-					// return 'M'+d.source.x+','+d.source.y+'L'+d.target.x+','+d.target.y;
-			})
-		    .style('marker-end', function(d) { return 'url(#end-arrow)'; });
-			// .style("stroke-width", function(d){ return Math.sqrt(d.value);});
+			.attr("x1", function(d) { 
+		  			return getEdgeCoord(centrality, d, score, d.source.x);
+  			})
+        	.attr("y1", function(d) { 
+	  			return getEdgeCoord(centrality, d, score, d.source.y);
+	  		})
+        	.attr("x2", function(d) { 
+	  			return getEdgeCoord(centrality, d, score, d.target.x);
+	  		})
+        	.attr("y2", function(d) { 
+	  			return getEdgeCoord(centrality, d, score, d.target.y);
+	  		})
+			.style("stroke-width", function(d){ return Math.sqrt(d.value);});
 
-	  	var node = svg.append("svg:g").selectAll("g")
+	  	var node = svg.selectAll(".node")
 	  		.data(graph.nodes)
 	  		.enter().append("circle")
 	  		.attr("class", "node")
