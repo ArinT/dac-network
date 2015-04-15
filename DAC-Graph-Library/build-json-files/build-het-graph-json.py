c8bde4fb40461e7b1b066851ddf65ab221b8dd7d
ethanyishchan <yish.chan@gmail.com>
2015-04-15 17:16:08 -0500

    edited rightsidebar.html

---
+++ DAC-Graph-Library/build-json-files/build-het-graph-json.py
@@ -0,0 +1,14 @@
+import numpy as np
+import json
+
+
+W_aptpa = np.load("W_aptpa.npy")
+W = W_aptpa
+W_ap = np.load ("W_ap.npy")
+W_ptp = np.load ("W_ptp.npy")
+
+
+#create new pathsim score matrix
+
+# print len(W)
+# exit()
